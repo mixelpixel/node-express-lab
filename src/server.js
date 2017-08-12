dc185bc0b5c7f55d734a10c78ad26e4245ce5756
pat collins <padjo@pats-MBP-2.fritz.box>
2017-08-12 08:12:33 +0200

    could it be that i have finished the post request. course not didnt run the post tests

--- src/server.js
+++ src/server.js
@@ -6,11 +6,27 @@ const STATUS_USER_ERROR = 422;
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
 const posts = [];
+let post = {};
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
 // TODO: your code to handle requests
+server.post('/posts', (req, res) => {
+  const title = req.body.title;
+  const contents = req.body.contents;
+  let id = posts.length;
+  if (!title || !contents) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ err: 'Must provide a title and content' });
+    return;
+  }
+  console.log(title, contents);
+  post = { id, title, contents };
+  posts.push(post);
+  res.send({ posts });
+});
+
 
 module.exports = { posts, server };
